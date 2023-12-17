a07e1e1d577995dfecfc9f7aaa35d96183fb6dc0
Dave Hardin <dave.hardin@hotmail.com>
2023-12-17 13:51:13 -0500

    edits

--- AIAgents/GridLoadMan/util/runanal.py
+++ AIAgents/GridLoadMan/util/runanal.py
@@ -13,39 +13,42 @@ def is_float(str_val):
 # Read the CSV file
 # Assuming the CSV file has columns: 'datetime', 'price', 'load'
 df_stats = pd.read_csv('..\\data\\LMP_LOAD_STATS.csv', parse_dates=['datetime'])
-df_stats_all = pd.read_csv('..\\data\\LMP_LOAD_STATS.csv', parse_dates=['datetime'])
-df_data = pd.read_csv('..\\data\\LMP_LOAD_META.csv', parse_dates=['datetime'])
-#df = pd.read_csv('stats.csv', parse_dates=['datetime'])
-
-# Set datetime as the index
 df_stats.set_index('datetime', inplace=True)
 df_stats.index = df_stats.index.date
 
-df_stats_all.set_index('datetime', inplace=True)
-
+df_data = pd.read_csv('..\\data\\LMP_LOAD_META.csv', parse_dates=['datetime'])
 df_data.set_index('datetime', inplace=True)
 df_data.index = df_data.index.date
+
+"""
+df_stats_all = pd.read_csv('..\\data\\LMP_LOAD_STATS.csv', parse_dates=['datetime'])
+df_stats_all.set_index('datetime', inplace=True)
+
+df_data_all = pd.read_csv('..\\data\\LMP_LOAD_META.csv', parse_dates=['datetime'])
+df_data_all.set_index('datetime', inplace=True)
+"""
 # Iterating over rows using iterrows()
-bronze_games = 0
-bronze_price = 200
+bronze_games_price = 0
+bronze_games_load = 0
+bronze_price = 100
 bronze_price_std = 50
 
-silver_games = 0
-silver_price = 225
+silver_games_price = 0
+silver_games_load = 0
+silver_price = 150
 silver_price_std = 25
 
 gold_games_price = 0
 gold_games_load = 0
-
 #250/150000
-
-gold_price = 250
+gold_price = 200
 gold_price_std = 10
 
-bronze_load = 130000
+
+bronze_load = 100000
 bronze_load_std = 15000
 
-silver_load = 140000
+silver_load = 120000
 silver_load_std = 18000
 
 gold_load=150000
@@ -77,29 +80,27 @@ for index, row in df_stats.iterrows():
                     print("price_min < 00 at " + str(my_datetime))
         """
         if column_name == "price_max":
-            if (is_float(value)):
-                if(float(value) > gold_price):
-                    if my_lastday != my_datetime:
-                        my_lastday = my_datetime
+            if my_lastday != my_datetime:
+                my_lastday = my_datetime
+                if (is_float(value)):
+                    if(float(value) > gold_price):
                         gold_games_price = gold_games_price + 1
                         print("alert gold_price at " + str(my_datetime))
                         std = row["price_std"]
                         mean = row["price_mean"]
                         load_max = row["load_max"]
                         load_std = row["load_std"]
-                        print("alert gold_price std " + str(std) + " " + str(mean))
+                        #print("alert gold_price std " + str(std) + " " + str(mean))
                         #
                         my_row =df_data.loc[my_datetime]
                         my_row_price = my_row["price"]
-                        print("real price " + str(my_row_price))
-                    else:
-                        pass
-                elif (float(value) > silver_price):
-                    #print("silver_price at " + str(my_datetime))
-                    silver_games = silver_games + 1
-                elif (float(value) > bronze_price):
-                    #print("bronze_price at " + str(my_datetime))
-                    bronze_games = bronze_games + 1
+                        #print("real price " + str(my_row_price))
+                    elif (float(value) > silver_price):
+                        #print("silver_price at " + str(my_datetime))
+                        silver_games_price = silver_games_price + 1
+                    elif (float(value) > bronze_price):
+                        #print("bronze_price at " + str(my_datetime))
+                        bronze_games_price = bronze_games_price + 1
 
 """
         if do_std == True:
@@ -141,28 +142,28 @@ for index, row in df_stats.iterrows():
         """
         
         if column_name == "load_max":
-            if (is_float(value)):
-                if(float(value) > gold_load):
-                    if my_lastday != my_datetime:
-                        my_lastday = my_datetime
+            if my_lastday != my_datetime:
+                my_lastday = my_datetime
+                if (is_float(value)):
+                    if(float(value) > gold_load):
                         gold_games_load = gold_games_load + 1
                         print("alert gold_load at " + str(my_datetime))
                         load_std = row["load_std"]
                         load_mean = row["load_mean"]
                         price_std = row["price_std"]
                         price_max = row["price_max"]
-                        print("alert gold_load std " + str(load_std) + " " + str(load_mean))
-                        print("alert gold_load price " + str(price_max))
-
+                        #print("alert gold_load std " + str(load_std) + " " + str(load_mean))
+                        #print("alert gold_load price " + str(price_max))
                         my_row =df_data.loc[my_datetime]
                         my_row_price = my_row["price"]
-                        print("real price " + str(my_row_price))
-            elif (float(value) > silver_load):
-                silver_games = silver_games + 1
-                #print("silver_load at " + str(my_datetime))
-            elif (float(value) > bronze_load):
-                #print("bronze_load at " + str(my_datetime))
-                bronze_games = bronze_games + 1
+                        #print("real price " + str(my_row_price))
+                    elif (float(value) > silver_load):
+                        silver_games_load = silver_games_load + 1
+                        #print("silver_load at " + str(my_datetime))
+                    elif (float(value) > bronze_load):
+                        #print("bronze_load at " + str(my_datetime))
+                        bronze_games_load = bronze_games_load + 1
+                        pass
 
 """
         if do_std == True:
@@ -185,6 +186,8 @@ for index, row in df_stats.iterrows():
 
 print ("gold_games_price = " + str(gold_games_price))
 print ("gold_games_load = " + str(gold_games_load))
-print ("silver_games = " + str(silver_games))
-print ("bronze_games = " + str(bronze_games))
+print ("silver_games_price = " + str(silver_games_price))
+print ("silver_games_load = " + str(silver_games_load))
+print ("bronze_games_price = " + str(bronze_games_price))
+print ("bronze_games_load = " + str(bronze_games_load))
 
