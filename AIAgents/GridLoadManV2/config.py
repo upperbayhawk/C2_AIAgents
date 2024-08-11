789ee705ce6705455b4255322be61c56931487a0
Dave Hardin <dave@upperbay.com>
2024-08-11 14:48:23 -0700

    edits

--- AIAgents/GridLoadManV2/config.py
+++ AIAgents/GridLoadManV2/config.py
@@ -12,7 +12,7 @@ import logbook
 
 agent_name = "GridLoadMan-2-0-0"
 # MQTT Broker settings. USE IP or "localhost".
-MQTT_BROKER = "192.168.0.114"
+MQTT_BROKER = "192.168.0.111"
 MQTT_PORT = 1883
 mqtt_base_topic = "openai/assistant/"
 mqtt_base_topic_c2agent = "C2Agent"
